4803bddb1ae709f86f40ff8bb4aaa4cd9efa6b2f
Даниил Откидышев <deni.oden@yandex.ru>
2023-05-07 13:59:54 +0700

    [200~

--- log_aggregator/log_aggregator/yasg.py
+++ log_aggregator/log_aggregator/yasg.py
@@ -7,17 +7,28 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Aggregator Apache Access Logs API",
       default_version='v1',
-      description="Test description",
-      license=openapi.License(name="BSD License"),
+      description="Aggregator Apache Access Logs",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path(r'^swagger(?P\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path(
+      r'^swagger(?P\.json|\.yaml)$',
+      schema_view.without_ui(cache_timeout=0),
+      name='schema-json',
+   ),
+   path(
+      'swagger/',
+      schema_view.with_ui('swagger', cache_timeout=0),
+      name='schema-swagger-ui',
+   ),
+   path(
+      'redoc/',
+      schema_view.with_ui('redoc', cache_timeout=0),
+      name='schema-redoc'
+   ),
 ]
